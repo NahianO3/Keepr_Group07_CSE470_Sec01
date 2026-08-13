0a4f6ed9a571fa56db95178050bfefbc88706a1f
MD. Nahian <mdnahiyan2628033@gmail.com>
2026-08-13 23:44:32 +0600

    updated maintenanceSchedule.py

--- Backend/app/models/MaintenanceSchedule.py
+++ Backend/app/models/MaintenanceSchedule.py
@@ -1,9 +1,20 @@
-""" MaintenanceSchedule Model """
+"""MaintenanceSchedule Model."""
 
 from masoniteorm.models import Model
+from masoniteorm.relationships import belongs_to
 
 
 class MaintenanceSchedule(Model):
-    """MaintenanceSchedule Model"""
 
-    pass
+    __fillable__ = [
+        "appliance_id",
+        "next_service_date",
+        "next_service_mileage",
+        "interval_days",
+        "reminder_enabled",
+    ]
+
+    @belongs_to("appliance_id", "id")
+    def appliance(self):
+        from app.models.Appliance import Appliance
+        return Appliance
