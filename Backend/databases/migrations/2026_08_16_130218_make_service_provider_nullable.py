925f14d17b122f067ededbcae338176ee94a5bb9
fahima <fahima.mehjabin@g.bracu.ac.bd>
2026-08-16 13:07:34 -0400

    add in migrations

--- Backend/databases/migrations/2026_08_16_130218_make_service_provider_nullable.py
+++ Backend/databases/migrations/2026_08_16_130218_make_service_provider_nullable.py
@@ -1,19 +1,14 @@
-"""MakeServiceProviderNullable Migration."""
+"""Make service_provider_id nullable."""
 
 from masoniteorm.migrations import Migration
 
 
 class MakeServiceProviderNullable(Migration):
+
     def up(self):
-        """
-        Run the migrations.
-        """
         with self.schema.table("maintenance_records") as table:
-            pass
+            table.integer("service_provider_id").unsigned().nullable().change()
 
     def down(self):
-        """
-        Revert the migrations.
-        """
         with self.schema.table("maintenance_records") as table:
-            pass
+            table.integer("service_provider_id").unsigned().change()
