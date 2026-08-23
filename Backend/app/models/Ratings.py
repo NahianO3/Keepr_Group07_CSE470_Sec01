0619032c4b70341c3a949636a3ed81fbb9ca05fa
Nazia Nawshin <nnawshin88@gmail.com>
2026-08-23 21:56:25 +0600

    rating and review model

---
+++ Backend/app/models/Ratings.py
@@ -0,0 +1,29 @@
+"""Rating Model for completed maintenance services."""
+
+from masoniteorm.models import Model
+from masoniteorm.relationships import belongs_to
+
+
+class Rating(Model):
+
+    __fillable__ = [
+        "customer_id",
+        "service_provider_id",
+        "maintenance_record_id",
+        "rating",
+    ]
+
+    @belongs_to("customer_id", "id")
+    def customer(self):
+        from app.models.User import User
+        return User
+
+    @belongs_to("service_provider_id", "id")
+    def service_provider(self):
+        from app.models.User import User
+        return User
+
+    @belongs_to("maintenance_record_id", "id")
+    def maintenance_record(self):
+        from app.models.MaintenanceRecord import MaintenanceRecord
+        return MaintenanceRecord
